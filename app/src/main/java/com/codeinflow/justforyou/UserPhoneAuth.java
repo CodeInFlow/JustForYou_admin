ac70a5e64e3a67fc424e36873430c947bd3546a1
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-12-11 23:26:23 +0530

    Add User Phone Auth Page

---
+++ app/src/main/java/com/codeinflow/justforyou/UserPhoneAuth.java
@@ -0,0 +1,20 @@
+package com.codeinflow.justforyou;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserPhoneAuth extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_user_phone_auth, container, false);
+
+        return v;
+    }
+}
