685783c113b0784b952c0a6c6f0b436f42261d12
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-12-13 18:19:13 +0530

    Add Java File for User Phone Auth

--- app/src/main/java/com/codeinflow/justforyou/UserPhoneAuth.java
+++ app/src/main/java/com/codeinflow/justforyou/UserPhoneAuth.java
@@ -2,19 +2,162 @@ package com.codeinflow.justforyou;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class UserPhoneAuth extends Fragment {
+
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    Map<String, Object> data = new HashMap<>();
+
+    EditText mobile, otp;
+
+    Button getotp;
+    BottomSheetDialog bottomSheetDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_user_phone_auth, container, false);
+        View v1 = inflater.inflate(R.layout.bottomsheet_user_verificationcode, container, false);
+
+        mobile = (EditText) v.findViewById(R.id.et_mobileno);
+        otp = v1.findViewById(R.id.et_otp);
+        getotp = v.findViewById(R.id.getotp);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        getotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mobile.getText().toString().length() != 10) {
+                    mobile.setError("Enter a valid mobile");
+                    mobile.requestFocus();
+                }
+                showBottomSheetDialog();
+            }
+        });
+
 
         return v;
     }
+
+    public void showBottomSheetDialog() {
+
+        sendVerificationCode(mobile.getText().toString());
+        bottomSheetDialog = new BottomSheetDialog(getActivity());
+        bottomSheetDialog.setContentView(R.layout.bottomsheet_user_verificationcode);
+        bottomSheetDialog.show();
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setCanceledOnTouchOutside(false);
+
+        Button login = bottomSheetDialog.findViewById(R.id.btn_login);
+        EditText otp = bottomSheetDialog.findViewById(R.id.et_otp);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = otp.getText().toString().trim();
+                if (code.isEmpty() || code.length() < 6) {
+                    otp.setError("Enter valid code");
+                    otp.requestFocus();
+                    return;
+                } else {
+                    verifyVerificationCode(code);
+
+                }
+            }
+
+        });
+    }
+
+    private void sendVerificationCode(String no) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + no,
+                60,
+                TimeUnit.SECONDS,
+                getActivity(),
+                mCallbacks);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            String code = phoneAuthCredential.getSmsCode();
+
+            if (code != null) {
+                verifyVerificationCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+            bottomSheetDialog.dismiss();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            mVerificationId = s;
+        }
+    };
+
+    private void verifyVerificationCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                            fragmentManager.beginTransaction().replace(R.id.fragment_container,new AddUserDetails());
+
+                        } else {
+
+
+                            String message = "Somthing is wrong, we will fix it soon...";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Invalid code entered...";
+
+                            }
+                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT);
+                            bottomSheetDialog.dismiss();
+
+
+                        }
+                    }
+                });
+    }
+
 }
