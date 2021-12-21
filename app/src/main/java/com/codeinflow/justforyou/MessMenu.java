658135893b4e3d53b092b220a85c45c64ca0af2f
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-12-21 21:42:32 +0530

    Add layout file for Menu page

---
+++ app/src/main/java/com/codeinflow/justforyou/MessMenu.java
@@ -0,0 +1,21 @@
+package com.codeinflow.justforyou;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MessMenu extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_mess_menu, container, false);
+
+
+        return v;
+    }
+}
